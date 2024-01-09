59bf07120659413689d5be422865594e547083db
Dave Huseby <dwh@linuxprogrammer.org>
2024-01-09 09:48:04 -0700

    add tweaks to deserializer
    Signed-off-by: Dave Huseby <dwh@linuxprogrammer.org>

--- src/serde/de.rs
+++ src/serde/de.rs
@@ -21,26 +21,69 @@ impl<'de> Deserialize<'de> for Codec {
                 )
             }
 
-            fn visit_string<E>(self, s: String) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                Ok(Codec::try_from(s.as_str()).map_err(|e| de::Error::custom(e.to_string()))?)
-            }
+            // human readable
 
-            fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                Ok(Codec::try_from(s).map_err(|e| de::Error::custom(e.to_string()))?)
-            }
-
+            // shortest lifetime
+            #[inline]
             fn visit_borrowed_str<E>(self, s: &'de str) -> Result<Self::Value, E>
             where
                 E: de::Error,
             {
-                Ok(Codec::try_from(s).map_err(|e| de::Error::custom(e.to_string()))?)
+                Ok(Self::Value::try_from(s).map_err(|e| de::Error::custom(e.to_string()))?)
             }
+
+            #[inline]
+            fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(Self::Value::try_from(s).map_err(|e| de::Error::custom(e.to_string()))?)
+            }
+
+            // longest lifetime
+            #[inline]
+            fn visit_string<E>(self, s: String) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(Self::Value::try_from(s.as_str())
+                    .map_err(|e| de::Error::custom(e.to_string()))?)
+            }
+
+            // binary
+
+            // shortest lifetime
+            #[inline]
+            fn visit_borrowed_bytes<E>(self, b: &'de [u8]) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(Self::Value::try_from(b).map_err(|e| de::Error::custom(e.to_string()))?)
+            }
+
+            #[inline]
+            fn visit_bytes<E>(self, b: &[u8]) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(Self::Value::try_from(b).map_err(|e| de::Error::custom(e.to_string()))?)
+            }
+
+            // longest lifetime
+            #[inline]
+            fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(Self::Value::try_from(v.as_slice())
+                    .map_err(|e| de::Error::custom(e.to_string()))?)
+            }
+
+            // binary / human readable
+
+            // this typically only happens when there are bytes serialized into
+            // a human readable format.
+            #[inline]
             fn visit_seq<S>(self, mut seq: S) -> Result<Self::Value, S::Error>
             where
                 S: de::SeqAccess<'de>,
@@ -49,28 +92,8 @@ impl<'de> Deserialize<'de> for Codec {
                 while let Some(b) = seq.next_element()? {
                     v.push(b);
                 }
-                Ok(Codec::try_from(v.as_slice()).map_err(|e| de::Error::custom(e.to_string()))?)
-            }
-
-            fn visit_bytes<E>(self, b: &[u8]) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                Ok(Codec::try_from(b).map_err(|e| de::Error::custom(e.to_string()))?)
-            }
-
-            fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                Ok(Codec::try_from(v.as_slice()).map_err(|e| de::Error::custom(e.to_string()))?)
-            }
-
-            fn visit_borrowed_bytes<E>(self, b: &'de [u8]) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                Ok(Codec::try_from(b).map_err(|e| de::Error::custom(e.to_string()))?)
+                Ok(Self::Value::try_from(v.as_slice())
+                    .map_err(|e| de::Error::custom(e.to_string()))?)
             }
         }
 
